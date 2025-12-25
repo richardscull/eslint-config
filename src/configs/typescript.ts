b6593e1b4e9288f55c8f7cb519f5f0cad9a14376
richardscull <106016833+richardscull@users.noreply.github.com>
2025-12-25 07:53:00 +0200

    feat: Add some specific rules

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -68,8 +68,8 @@ export function typeScriptConfigs({
           }
         : {
             rules: {
-              "@typescript-eslint/ban-ts-comment": "off",
-              "@typescript-eslint/no-explicit-any": "off",
+              "@typescript-eslint/ban-ts-comment": "error",
+              "@typescript-eslint/no-explicit-any": "warn",
               "@typescript-eslint/no-non-null-asserted-optional-chain": "warn",
             },
           },
